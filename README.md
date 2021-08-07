d51b3fd5bf5170f4a46ab93c8daa25d0277eb57a
systemb4 <lukasnitzsche@yahoo.com>
2021-08-07 13:30:47 +0300

    swap from keys.h(local) to linux/input-event-codes.h(global)

---
+++ README.md
@@ -0,0 +1,2 @@
+# Suckless Hot Keys
+Although not suckless in theory, the configuration of this applicaiton is meant to replicate suckless programs. This program just looks at the keyboard input event and runs actions based on the type of key and what the user specifies.
--- main.c
+++ main.c
@@ -7,9 +7,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdbool.h>
-#include "keys.h"
+#include <linux/input-event-codes.h>
 
 #define EVENT_FILE "/dev/input/event3"
+#define EVCODE_DEPRESS 1
+#define EVCODE_RELEASE 0
 
 int main(int argc, char **argv) {
     struct input_event ev;
@@ -24,14 +26,13 @@ int main(int argc, char **argv) {
     while(con) {
         read(fd, &ev, sizeof(ev));
         fflush(stdout);
-        if(ev.value == evcodeRelease)
-            printf("%i\n", ev.code);
-        /*
-        if(ev.value == evcodeDepress) {
-            if(ev.code == keyGrave && ev.code == keyF3) {
+        //if(ev.value == evcodeRelease)
+         //   printf("%i\n", ev.code);
+        if(ev.value == 1) {
+            if(ev.code == KEY_GRAVE) {
                 system(command);
             }
-        }*/
+        }
     }
     close(fd);
 

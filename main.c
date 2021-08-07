f4c91e7c025aac8eeb7859eb05cbbac19aceea39
systemb4 <lukasnitzsche@yahoo.com>
2021-08-07 13:53:30 +0300

    work with volume keys

--- main.c
+++ main.c
@@ -4,32 +4,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/input.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdbool.h>
 #include <linux/input-event-codes.h>
 
 #define EVENT_FILE "/dev/input/event3"
 #define EVCODE_DEPRESS 1
 #define EVCODE_RELEASE 0
 
+/*
+ * create struct to take in key and command
+ * and create function to read struct and run through if statment in while loop
+ * create config.def.h and custom rc file in config folder later
+ * */
+
 int main(int argc, char **argv) {
     struct input_event ev;
-    bool con = true;
     int fd = open(EVENT_FILE, O_RDONLY);
 
-    // char *map = "..1234567890-=..qwertyuiop[]\n.asdfghjkl;'~^\\zxcvbnm,./^.. .";
-    // printf("%c", map[ev.code]);
-
-    char command[10];
-    strcpy(command, "st &");
-    while(con) {
+    char command[30];
+    strcpy(command, "amixer -q sset Master 3%-");
+    while(1) {
         read(fd, &ev, sizeof(ev));
         fflush(stdout);
-        //if(ev.value == evcodeRelease)
-         //   printf("%i\n", ev.code);
-        if(ev.value == 1) {
-            if(ev.code == KEY_GRAVE) {
+        if(ev.value == EVCODE_DEPRESS) {
+            if(ev.code == KEY_VOLUMEDOWN) {
                 system(command);
             }
         }
